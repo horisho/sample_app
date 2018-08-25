35de563e7e5dcb190088c00b1e00cf8de70fa2a4
horisho <oh.yes.i.know@gmail.com>
2018-08-25 09:54:30 +0900

    rackup　DefaultRackupをコメントアウト

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
@@ -94,5 +94,4 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, user Strict-Transport-Security,
   # and user secure cookies.
-  config.force_ssl = true
 end
